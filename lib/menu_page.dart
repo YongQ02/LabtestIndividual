fe908c99c96dec7b0d83194420493f63555faf62
YongQin <93411896+YongQ02@users.noreply.github.com>
2024-09-10 12:04:34 +0800

    Update menu_page.dart

--- lib/menu_page.dart
+++ lib/menu_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'homepage.dart'; // 确保导入这个文件
+import 'homepage.dart'; 
 
 class MenuPage extends StatefulWidget {
   @override
@@ -13,10 +13,10 @@ class _MenuPageState extends State<MenuPage> {
   bool isLoading = true;
   Map<String, dynamic>? movieData;
   final String apiKey = 'bd3300f908fc6afc85a12790fd1dcfe1'; 
-  String currentQuery = 'horror'; // 当前查询类型
-  List<String> queries = ['horror', 'action', 'comedy', 'drama']; // 电影类型列表
-  int currentIndex = 0; // 当前电影类型索引
-
+  String currentQuery = 'horror'; 
+  List<String> queries = ['horror', 'action', 'comedy', 'drama']; 
+  int currentIndex = 0; 
+  
   @override
   void initState() {
     super.initState();
@@ -32,7 +32,7 @@ class _MenuPageState extends State<MenuPage> {
         final data = json.decode(response.body);
         setState(() {
           movieData = data['results'] != null && data['results'].isNotEmpty
-              ? data['results'][0] // 获取第一个电影结果
+              ? data['results'][0]
               : null;
           isLoading = false;
         });
@@ -50,7 +50,7 @@ class _MenuPageState extends State<MenuPage> {
   void _changeMovie() {
     setState(() {
       isLoading = true;
-      currentIndex = (currentIndex + 1) % queries.length; // 循环切换电影类型
+      currentIndex = (currentIndex + 1) % queries.length; 
       currentQuery = queries[currentIndex];
     });
     fetchMovieData(currentQuery);
@@ -131,10 +131,10 @@ class _MenuPageState extends State<MenuPage> {
                   onPressed: _changeMovie,
                   child: Text(
                     'Change Movie',
-                    style: TextStyle(color: Colors.white), // 改变按钮文本颜色
+                    style: TextStyle(color: Colors.white), 
                   ),
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.indigo, // 改变按钮背景颜色
+                    backgroundColor: Colors.indigo, 
                     padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
                     textStyle: TextStyle(fontSize: 18),
                   ),
@@ -147,49 +147,3 @@ class _MenuPageState extends State<MenuPage> {
     );
   }
 }
-
-
-/*class MenuPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Menu Page'),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.indigo,
-              ),
-              child: Text(
-                'Menu',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.exit_to_app),
-              title: Text('Logout'),
-              onTap: () async {
-                await FirebaseAuth.instance.signOut();
-                Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomePage()),
-                  (route) => false,
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-      body: Center(
-        child: Text('Welcome to the Menu Page!'),
-      ),
-    );
-  }
-}*/
